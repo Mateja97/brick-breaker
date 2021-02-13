0790794307847a8a008d0b2d4f74687b1954ac40
Mateja97 <matejarkvc@gmail.com>
2021-02-13 22:57:34 +0100

    small updates

--- states/gameover.lua
+++ states/gameover.lua
@@ -1,9 +1,11 @@
 local Gameover = {}
 
 function Gameover:draw()
+    love.graphics.setColor(1,0,0)
+    love.graphics.rectangle("fill",love.graphics.getWidth()*0.3,love.graphics.getHeight()*0.3,500,300)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print("GAME OVER" , love.graphics.getWidth()*0.4,love.graphics.getHeight()*0.4)
-    love.graphics.print("Press R to restart the game" , love.graphics.getWidth()*0.4,love.graphics.getHeight()*0.5)
+    love.graphics.print("GAME OVER" , love.graphics.getWidth()*0.4 + 30,love.graphics.getHeight()*0.4)
+    love.graphics.print("Press R to restart the game" , love.graphics.getWidth()*0.4-50,love.graphics.getHeight()*0.5)
 end
 
 return Gameover
--- states/win.lua
+++ states/win.lua
@@ -1,6 +1,8 @@
 local win = {}
 
 function win:draw()
+    love.graphics.setColor(0,1,0)
+    love.graphics.rectangle("fill",love.graphics.getWidth()*0.3 -100,love.graphics.getHeight()*0.3,750,300)
     love.graphics.setColor(1, 1, 1)
     love.graphics.print("Congratulations you have passed the level!" , love.graphics.getWidth()*0.3,love.graphics.getHeight()*0.4)
     love.graphics.print("Press R to restart the game or Esc to menu" , love.graphics.getWidth()*0.3,love.graphics.getHeight()*0.5)
